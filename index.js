e9164a1df22f02545c6468e63a836973d7c2e4e2
Roman Bohos <132600951+Bohousek007@users.noreply.github.com>
2023-12-04 15:07:05 +0100

    add files
    just import files

---
+++ index.js
@@ -0,0 +1,13 @@
+import { ahoj } from "./src/ahoj.js"
+import { addTask } from "./src/addTask.js"
+import { loadTask } from './src/loadTask.js'
+import { deleteTask } from './src/deleteTask.js'
+import { editTask } from './src/editTask.js'
+import { saveTasks } from "./src/saveTask.js"
+
+
+console.log(addTask)
+console.log(loadTask)
+console.log(deleteTask)
+console.log(editTask)
+console.log(saveTasks)
